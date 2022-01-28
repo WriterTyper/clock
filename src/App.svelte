fc9e93b6941a7295276f738956fd7c07b7f9e168
scholarcraft <scholarcraft.io@outlook.com>
2022-01-27 22:17:32 -0700

    Reconfigure

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import {onMount} from 'svelte';
+	let time = new Date();
+	$: hours = time.getHours();
+	$: minutes = time.getMinutes();
+	$: seconds = time.getSeconds();
+	
+	onMount(() => {
+		const interval = setInterval(() => {
+			time = new Date();
+		}, 1000);
+
+		return () => {
+			clearInterval(interval);
+		};
+	});
+	
+</script>
+
+<h1>A Clock of Time</h1>
+<h3> What if time is reimagined this way. Because seconds turn to minutes, minutes to hours, hours to days.</h3>
+<main>
+<h2>	 {seconds} : {minutes} : {hours} </h2>
+</main>
